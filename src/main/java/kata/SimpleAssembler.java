12f19eb65061bf65fe2396e43f635289fb0d9bac
alexandrov <alexandrov@o-code.com>
2022-08-23 10:44:27 +0400

    Fix SimpleAssembler kata

--- src/main/java/kata/SimpleAssembler.java
+++ src/main/java/kata/SimpleAssembler.java
@@ -2,45 +2,48 @@ package kata;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public final class SimpleAssembler {
 
+    interface Command {
+        void execute(Map<String, Integer> variables, Command[] commands, int index);
+    }
 
     public static Map<String, Integer> interpret(String[] program){
-        Map<String, Integer> vars = new HashMap<>();
-        List<Command> commands = Arrays.stream(program)
+        Map<String, Integer> variables = new HashMap<>();
+        Command[] commands = Arrays.stream(program)
                 .map(SimpleAssembler::toCommand)
-                .toList();
-        for(int i = 0; i < commands.size(); i++)
-            commands.get(i).execute(vars, commands, i);
-        return vars;
+                .toArray(Command[]::new);
+        executeAll(variables, commands, 0, commands.length);
+        return variables;
     }
 
     private static Command toCommand(String statement) {
         String[] lexems = statement.split(" ");
         return switch(lexems[0]) {
-            case "mov" -> (vars, commands, i) -> {
-                int value = lexems[2].matches("-?\\d+") ? Integer.parseInt(lexems[2]) : vars.get(lexems[2]);
-                vars.put(lexems[1], value);
-            };
-            case "jnz" -> (vars, commands, i) -> {
-                int target = i + Integer.parseInt(lexems[2]);
-                var loop = (target < i)
-                        ? commands.subList(target, i)
-                        : commands.subList(i, target);
-                while(vars.get(lexems[1]) != 0)
-                    loop.forEach(command -> command.execute(vars, commands, i));
-            };
+            case "mov" -> (vars, commands, i) -> vars.put(lexems[1], getOrParse(vars, lexems[2]));
             case "inc" -> (vars, commands, i) -> vars.merge(lexems[1], 1, Integer::sum);
             case "dec" -> (vars, commands, i) -> vars.merge(lexems[1], -1, Integer::sum);
+            case "jnz" -> (vars, commands, i) -> {
+                int target = i + Integer.parseInt(lexems[2]);
+                while(getOrParse(vars, lexems[1]) != 0)
+                    executeAll(vars, commands, min(target, i), max(target, i));
+            };
             default -> throw new IllegalStateException("Unexpected statement: " + statement);
         };
     }
 
-    interface Command {
-        void execute(Map<String, Integer> variables, List<Command> allCommands, int index);
+    private static int getOrParse(Map<String, Integer> variables, String lexem) {
+        Integer value = variables.get(lexem);
+        return (value != null) ? value : Integer.parseInt(lexem);
     }
 
+    private static void executeAll(Map<String, Integer> variables, Command[] commands, int index, int bound) {
+        for( ; index < bound; index++)
+            commands[index].execute(variables, commands, index);
+    }
 }
